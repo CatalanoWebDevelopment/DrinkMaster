aabdefec3d4f633229dec083ee899008ddd3356d
catalanowebdevelopment <catalano.work@gmail.com>
2018-04-11 16:34:12 -0400

    Fixed Log In and Log Out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
     devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
     
-    devise_for :users
-    
     root to: "welcome#home"
     
     resources :users, only: [:show, :edit]
