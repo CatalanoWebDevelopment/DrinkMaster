e64897feccb3fd90c8748e83ea40b2fe5674a024
catalanowebdevelopment <catalano.work@gmail.com>
2018-07-10 10:14:20 -0400

    Added a Back to Profile Button

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -38,7 +38,6 @@ class RecipesController < ApplicationController
     def update
         @recipe = Recipe.find(params[:id])
         
-        
         if @recipe.update(recipe_params)
             redirect_to recipe_path(@recipe), notice: ('Your Recipe Was Succesfully Updated')
         else
